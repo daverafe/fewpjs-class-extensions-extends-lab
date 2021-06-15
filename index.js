de54058987f12c7df72930cb318def1a181d67be
David Rafe <david.rafe@gmail.com>
2021-06-15 13:04:01 -0400

    commit tree 41a1ab74ebf7a6ce4d4fd2e75645bd9fedd13884

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+class Polygon {
+    constructor(integers) {
+        this.integers = integers
+    }
+
+    get countSides() {
+        return this.integers.length 
+    }
+
+    get perimeter() {
+        return this.integers.reduce(function(total, integer) {
+            return total + integer
+        })
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        let intArray = this.integers
+        if((intArray[0] + intArray[1]) < intArray[2]) {
+            return false 
+        } else if((intArray[1] + intArray[2]) < intArray[0]) {
+            return false 
+        } else if((intArray[0] + intArray[2]) < intArray[1]) {
+            return false 
+        } else {
+            return true 
+        }
+    }
+}
+
+class Square extends Polygon {
+    get isValid() {
+        return new Set(this.integers).size == 1 
+    }
+
+    get area() {
+        if(this.isValid) {
+            return this.integers[0] * this.integers[1]
+        }
+    }
+}
